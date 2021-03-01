aa73fdcfee70a80dbf37ab0d00d715ea5839d3c4
MadanNV001 <76660117+MadanNV001@users.noreply.github.com>
2021-03-01 15:42:44 +0530

    Part 2: Solution
    Part 2: Solution

--- Restaurant_project -Student version/src/test/java/RestaurantServiceTest.java
+++ Restaurant_project -Student version/src/test/java/RestaurantServiceTest.java
@@ -67,14 +67,4 @@ class RestaurantServiceTest {
     }
     //<<<<<<<<<<<<<<<<<<<<ADMIN: ADDING & REMOVING RESTAURANTS>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-    //requirements
-    //select the food
-    //add the cost
-   @Test
-    public void Select_the_Food_item_and_find__total_cost_of_the_order(){
-      Receipt receipt =service.totalCost("Vegetable lasagne", 269,1);
-       assertEquals(269,receipt.getPrice());
-     assertNotNull(receipt);
-
-   }
 }
